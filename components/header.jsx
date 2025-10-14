00d77795fc592503b30dc8fed8d8f156677d53ef
Athrv16 <atharvakantode16@gmail.com>
2025-10-15 00:15:29 +0530

    Logo change

--- components/header.jsx
+++ components/header.jsx
@@ -11,7 +11,7 @@ const Header = async () => {
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
           <Image
-            src="/public/logo.png"
+            src="public/logo.png"
             alt="finan logo"
             height={90}
             width={300}
